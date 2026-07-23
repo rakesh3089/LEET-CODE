854dd7c45f23c5e0edd391abf8b3f87597a67cd3
rakesh3089 <rakeshkattaboyina@gmail.com>
2026-07-23 08:27:33 +0530

    Implement binary search for square root calculation

---
+++ sqrt.69.java
@@ -0,0 +1,27 @@
+lass Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        long left = 1;
+        long right = x / 2;
+        long ans = 0;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == x) {
+                return (int) mid;
+            } else if (square < x) {
+                ans = mid; 
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return (int) ans;
+    }
+}
