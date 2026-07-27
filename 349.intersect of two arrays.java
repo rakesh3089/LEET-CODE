fb7d63041c8cd011654cd29a995c3fce0479bfb7
rakesh3089 <rakeshkattaboyina@gmail.com>
2026-07-27 11:07:08 +0530

    Create 349.intersect of two arrays.java

---
+++ 349.intersect of two arrays.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> intersectSet = new HashSet<>();
+        
+        for (int num : nums1) {
+            set1.add(num);
+        }
+        
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersectSet.add(num);
+            }
+        }
+        
+        int[] result = new int[intersectSet.size()];
+        int i = 0;
+        for (int num : intersectSet) {
+            result[i++] = num;
+        }
+        
+        return result;
+    }
+}
