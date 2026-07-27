cb0cfead54a7fb645a14eb80104ce9bd14f99f95
rakesh3089 <rakeshkattaboyina@gmail.com>
2026-07-27 12:11:22 +0530

    Implement string to integer conversion (atoi)

---
+++ 8. String to Integer (atoi).java
@@ -0,0 +1,36 @@
+class Solution {
+    public int myAtoi(String s) {
+        int index = 0;
+        int n = s.length();
+        int sign = 1;
+        int result = 0;
+        
+        while (index < n && s.charAt(index) == ' ') {
+            index++;
+        }
+        
+        if (index == n) {
+            return 0;
+        }
+        
+        // 2. Determine sign
+        if (s.charAt(index) == '+' || s.charAt(index) == '-') {
+            sign = (s.charAt(index) == '-') ? -1 : 1;
+            index++;
+        }
+        
+        while (index < n && Character.isDigit(s.charAt(index))) {
+            int digit = s.charAt(index) - '0';
+            
+            if (result > Integer.MAX_VALUE / 10 || 
+                (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            
+            result = result * 10 + digit;
+            index++;
+        }
+        
+        return result * sign;
+    }
+}
